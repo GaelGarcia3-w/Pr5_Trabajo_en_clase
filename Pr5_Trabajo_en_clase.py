7f6142dd529fec2059156e24d1acefe8a6ec8544
Galletas <a.23308051281182@cbtis128.edu.mx>
2024-09-24 19:56:53 -0600

    Pr5_Trabajo_en_clase

---
+++ Pr5_Trabajo_en_clase.py
@@ -0,0 +1,17 @@
+print(" ")
+
+print("Edgar Gael Garcia Camacho :Pr5_Trabajo_en_clase")
+
+print(" ")
+
+num1=int(input("Selecciona el primer numero :"))#Te pide ingresar un numero.
+
+num2=int(input("Selecciona el segundo numero :"))#Te pide ingresar un numero.
+
+num3=int(input("Selecciona el tercer numero :"))#Te pide ingresar un numero.
+
+suma= (num1 + num2 + num3)#La suma de los 3 mumeros seleccionados.
+
+r= (suma /3)#La suma que se realizo anteriormente se divide entre 3.
+
+print("El promedio conseguido es :", r)#Al final te saltara el mensaje con el promedio.
